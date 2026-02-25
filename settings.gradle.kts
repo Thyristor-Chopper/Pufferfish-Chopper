5e727ebdccf861f4ce570c6a7b91c1ea33102794
싸이리스터 초퍼 <gec_thyristor_chopper@[127.0.0.1]>
2026-02-26 03:15:16 +0900

    v. 20260226

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+rootProject.name = "chopper"
+include("chopper-api", "chopper-server")
